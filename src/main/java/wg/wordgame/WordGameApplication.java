48489e594e30aabd27a1f0852cb4cd4d97c6255c
Emilija Drazic <drazicemilija3@gmail.com>
2022-05-12 00:38:16 +0200

    added project

---
+++ src/main/java/wg/wordgame/WordGameApplication.java
@@ -0,0 +1,13 @@
+package wg.wordgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordGameApplication.class, args);
+	}
+
+}
